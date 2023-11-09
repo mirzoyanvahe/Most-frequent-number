03cd17b3e76bc87f22c559bb791256997f59f9d2
mirzoyanvahe <121468077+mirzoyanvahe@users.noreply.github.com>
2023-11-09 23:10:18 +0400

    Create bst.c

---
+++ bst.c
@@ -0,0 +1,14 @@
+#include "bst.h"
+#include <stdlib.h>
+
+struct Node* insert(struct Node* root, int value) {
+}
+
+struct Node* search(struct Node* root, int value) {
+}
+
+void inorderTraversal(struct Node* root) {
+}
+
+int findMostFrequent(struct Node* root) {
+}
